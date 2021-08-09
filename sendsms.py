ce2365231bda38a67cf94aae5f8331b7453fd31a
johnbartosiii <rbean_44@yahoo.com>
2021-08-09 12:13:05 -0400

    remove stuff

--- sendsms.py
+++ sendsms.py
@@ -18,14 +18,9 @@ def send(number, carrier, message):
 	auth = (sms_email, sms_email_pass)
 
 	# Establish a secure session with gmail's outgoing SMTP server using your gmail account
-	#try:
 	context = ssl.create_default_context()
 	server = smtplib.SMTP_SSL( "smtp.gmail.com", 465, context=context)
 	server.login(auth[0], auth[1])
 	server.sendmail(auth[0], to_number, message)
-	#except:
-	#	print('Something went wrong.')
 
 
-if __name__ == '__main__':
-	send('4845466368', 'att', 'testing...')
